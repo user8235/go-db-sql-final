345130ba99c7a808b4be5ac407ca8f109b3efc61
user8235 <phone192@yandex.ru>
2024-12-11 00:33:05 +0300

    3

--- parcel.go
+++ parcel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type ParcelStore struct {
@@ -14,17 +15,31 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
-
+	res, err := s.Exec("INSERT INTO parcel (number, client, status, address, created_at) VALUES (:number, :client, :status, :address, :created_at)",
+		sql.Named("number,", p.Number),
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt))
+	if err != nil {
+		fmt.Println("Error during 'Add' operation:", err)
+		return 0, nil
+	}
 	// верните идентификатор последней добавленной записи
-	return 0, nil
+	return res.LastInsertId(), nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
 	// реализуйте чтение строки по заданному number
 	// здесь из таблицы должна вернуться только одна строка
-
+	row := s.QueryRow("SELECT number, client, status, address, created_at FROM products WHERE id = :id", sql.Named("id", number))
 	// заполните объект Parcel данными из таблицы
 	p := Parcel{}
+	err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+	if err != nil {
+		fmt.Println("Error during 'Get' operation:", err)
+		return p, err
+	}
 
 	return p, nil
 }
@@ -32,9 +47,27 @@ func (s ParcelStore) Get(number int) (Parcel, error) {
 func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
 	// реализуйте чтение строк из таблицы parcel по заданному client
 	// здесь из таблицы может вернуться несколько строк
-
+	rows, err := s.Query("SELECT number, client, status, address, created_at FROM products WHERE id = :id", sql.Named("id", client))
+	if err != nil {
+		fmt.Println("Error during 'GetByClient' operation:", err)
+		return nil, err
+	}
+	defer rows.Close()
 	// заполните срез Parcel данными из таблицы
 	var res []Parcel
+	for rows.Next() {
+		// var (
+		// 	id int
+		// 	product string
+		// 	price int
+		// )
+
+		err := rows.Scan(&res.Number, &res.Client)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 
 	return res, nil
 }
